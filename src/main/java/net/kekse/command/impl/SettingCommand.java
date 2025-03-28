ea6fd61f67f498ba6067bdd112d1ed3f6b5c492a
Vxrtrauter <gmakberh@gmail.com>
2025-03-28 02:35:16 +0100

    Refactor SettingCommand and SettingManager for improved usability and clarity + Added Case Insensitivity for Commands

--- src/main/java/net/kekse/command/impl/SettingCommand.java
+++ src/main/java/net/kekse/command/impl/SettingCommand.java
@@ -13,13 +13,10 @@ import net.kekse.util.ChatUtil;
 
 @CommandInfo(
         name = "setting",
-        usage = "setting <command> <Setting> <Value>",
+        usage = "setting <module> <setting> <value>",
         description = "Change settings for modules",
         aliases = {"s", "set", "settings"}
-
 )
-
-
 public class SettingCommand extends Command {
     @Override
     public void execute(String... args) throws CommandException {
@@ -30,16 +27,18 @@ public class SettingCommand extends Command {
 
         Module mod = KeksKlient.INSTANCE.getMm().getModule(args[0]);
 
-        if (args.length == 1 && mod == null) {
+        if (mod == null) {
             ChatUtil.addChatMessage("&cModule not found");
             return;
         }
+
 
         if (args.length == 1) {
             if (mod.getSList().isEmpty()) {
                 ChatUtil.addChatMessage("&fModule &b" + mod.getName() + "&f has no settings");
                 return;
             }
+
             ChatUtil.addChatMessage("&fAvailable settings for &b" + mod.getName() + "&f:");
             for (Setting setting : KeksKlient.INSTANCE.getSm().getSettingsForModule(mod)) {
                 String type = "";
@@ -64,56 +63,59 @@ public class SettingCommand extends Command {
         }
 
 
-
-        if (args.length != 3 && args.length != 6) {
+        if (args.length != 3) {
             ChatUtil.addChatMessage("&cUsage: &f" + getUsage());
             return;
         }
 
-        Setting setting = KeksKlient.INSTANCE.getSm().getSetting(mod, args[1]);
 
+        Setting setting = KeksKlient.INSTANCE.getSm().getSetting(mod, args[1]);
         if (setting == null) {
             ChatUtil.addChatMessage("&cSetting not found");
             return;
         }
 
         try {
+
             if (setting instanceof BooleanSetting) {
                 BooleanSetting bs = (BooleanSetting) setting;
-
-                if (!(args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("false"))) {
-                    ChatUtil.addChatMessage("&fYou must either give &atrue&r &for &cfalse &fvalue");
-                    return;
-                }
-
-                bs.setState(args[2].equalsIgnoreCase("true"));
+                bs.setState(Boolean.parseBoolean(args[2].toLowerCase()));
+                ChatUtil.addChatMessage("&fSet &b" + mod.getName() + "&f's &b" + setting.getName() + "&f to &b" + bs.isState());
+                return;
             }
 
             if (setting instanceof DoubleSetting) {
                 DoubleSetting ds = (DoubleSetting) setting;
-                double myDouble = Double.parseDouble(args[2]);
-                if (myDouble > ds.getMaxVal()) {
+                double value = Double.parseDouble(args[2]);
+                if (value > ds.getMaxVal()) {
                     ChatUtil.addChatMessage("&cError: &fValue exceeds maximum value");
                     return;
                 }
-                ds.setVal(myDouble);
-                ChatUtil.addChatMessage("Set &b" + mod.getName() + "&f's &b" + setting.getName() + "&f to &b" + args[2]);
+                if (value < ds.getMinVal()) {
+                    ChatUtil.addChatMessage("&cError: &fValue is below minimum value");
+                    return;
+                }
+                ds.setVal(value);
+                ChatUtil.addChatMessage("&fSet &b" + mod.getName() + "&f's &b" + setting.getName() + "&f to &b" + value);
                 return;
             }
 
             if (setting instanceof ModeSetting) {
                 ModeSetting ms = (ModeSetting) setting;
+                String targetMode = ms.getModes().stream()
+                        .filter(mode -> mode.equalsIgnoreCase(args[2]))
+                        .findFirst()
+                        .orElse(null);
 
-                if (ms.getModes().stream().noneMatch(s -> s.equalsIgnoreCase(args[2]))) {
+                if (targetMode == null) {
                     ChatUtil.addChatMessage("&cSpecified mode was not found");
                     return;
                 }
-                ms.setCurrentMode(args[2]);
-                ChatUtil.addChatMessage("Set &b" + mod.getName() + "&f's &b" + setting.getName() + "&f to &b" + args[2]);
+                ms.setCurrentMode(targetMode);
+                ChatUtil.addChatMessage("Set &b" + mod.getName() + "&f's &b" + setting.getName() + "&f to &b" + targetMode);
             }
-
         } catch (NumberFormatException e) {
-            ChatUtil.addChatMessage("&cYou must pass a number as an Argument/Invalid Number!");
+            ChatUtil.addChatMessage("&cYou must pass a valid number as an argument!");
         }
     }
 }
--- src/main/java/net/kekse/settings/SettingManager.java
+++ src/main/java/net/kekse/settings/SettingManager.java
@@ -5,55 +5,53 @@ import net.kekse.settings.impl.BooleanSetting;
 import net.kekse.settings.impl.DoubleSetting;
 import net.kekse.settings.impl.ModeSetting;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SettingManager {
-
-    public List<Setting> settingsList;
+    private final List<Setting> settingsList = new ArrayList<>();
 
     public BooleanSetting getBooleanSetting(Module mod, String settingName) {
-        for(Setting setting : mod.getSList()) {
+        for (Setting setting : mod.getSList()) {
             if (setting instanceof BooleanSetting) {
                 BooleanSetting bs = (BooleanSetting) setting;
-
-                if(bs.getName().equals(settingName))
+                if (bs.getName().equalsIgnoreCase(settingName)) {
                     return bs;
+                }
             }
         }
         return null;
     }
 
-    public ModeSetting getModeSetting (Module mod, String settingName) {
-        for(Setting setting : mod.getSList()) {
-            if(setting instanceof  ModeSetting) {
+    public ModeSetting getModeSetting(Module mod, String settingName) {
+        for (Setting setting : mod.getSList()) {
+            if (setting instanceof ModeSetting) {
                 ModeSetting ms = (ModeSetting) setting;
-
-                if(ms.getName().equals(settingName))
+                if (ms.getName().equalsIgnoreCase(settingName)) {
                     return ms;
+                }
             }
         }
-    return null;
+        return null;
     }
 
-
     public DoubleSetting getDoubleSetting(Module mod, String settingName) {
-        for(Setting setting : mod.getSList()) {
-            if(setting instanceof DoubleSetting) {
+        for (Setting setting : mod.getSList()) {
+            if (setting instanceof DoubleSetting) {
                 DoubleSetting ds = (DoubleSetting) setting;
-
-                if(ds.getName().equals(settingName))
+                if (ds.getName().equalsIgnoreCase(settingName)) {
                     return ds;
+                }
             }
         }
         return null;
     }
 
     public Setting getSetting(Module mod, String settingName) {
-        for(Setting setting : mod.getSList()) {
-            if(setting.getName().equals(settingName)) {
+        for (Setting setting : mod.getSList()) {
+            if (setting.getName().equalsIgnoreCase(settingName)) {
                 return setting;
             }
-
         }
         return null;
     }
@@ -62,4 +60,15 @@ public class SettingManager {
         return mod.getSList();
     }
 
+    public void registerSetting(Setting setting) {
+        this.settingsList.add(setting);
+    }
+
+    public void unregisterSetting(Setting setting) {
+        this.settingsList.remove(setting);
+    }
+
+    public void clearSettings() {
+        this.settingsList.clear();
+    }
 }
